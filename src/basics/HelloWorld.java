2f044f9d49fbf41f8c472470e89233eedc8f407a
cvalle21 <41059738+cvalle21@users.noreply.github.com>
2018-07-10 09:43:08 -0400

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Adding code to my project to test GIT changes");
+		System.out.println("Adding code from GITHub to clone to Eclipse");
 	}
 }
